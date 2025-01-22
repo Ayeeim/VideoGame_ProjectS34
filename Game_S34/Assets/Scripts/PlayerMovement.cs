2cdfb3b5f2944c4e7e140c58b5beb736dabf6fcf
Ayeeim <yaya140105@gmail.com>
2025-01-23 00:11:43 +0100

    Update on PlayerMovement
    Update on PlayerMovement script, character movement and running has been added, camera follow (with cinemachine) has been added too (added collision test too)

--- Game_S34/Assets/Scripts/PlayerMovement.cs
+++ Game_S34/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody2D rb;
+
+    public bool isSprinting = false;
+
+    public float movementSpeed = 5f;
+    public float sprintingSpeed =  8f;
+
+
+    Vector2 dir;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        dir.x = Input.GetAxis("Horizontal");
+        dir.y = Input.GetAxis("Vertical");
+
+        rb.MovePosition(rb.position + dir * movementSpeed * Time.fixedDeltaTime);
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            isSprinting= true;
+            movementSpeed = sprintingSpeed;
+        }
+        else
+        {
+            isSprinting = false;
+            movementSpeed = 5f;
+        }
     }
 }
