e47b6d97838f407ac5bddfb2c436ed200a8dc67d
Ayeeim <yaya140105@gmail.com>
2025-04-10 00:25:52 +0200

    Change and reapir pickUp system
    pickUp system now works

--- Game_S34/Assets/Scripts/PickUpObject.cs
+++ Game_S34/Assets/Scripts/PickUpObject.cs
@@ -6,56 +6,36 @@ public class PickUpObject : MonoBehaviour
 {
 
     public InputActionReference interAction;
-
     public bool interactUi = false;
-    public TextMeshProUGUI textInteraction;
 
-    private void Awake()
+
+    private void Update()
     {
-        textInteraction = GameObject.FindGameObjectWithTag("InteractUI").GetComponent<TextMeshProUGUI>();
-    }
-
-    private void OnEnable()
-    {
-        interAction.action.started += onInterActionStarted;
-        interAction.action.Enable();
-
-    }
-
-    private void OnDisable()
-    {
-        interAction.action.started -= onInterActionStarted;
-        interAction.action.Disable();
-    }
-
-    private void onInterActionStarted(InputAction.CallbackContext obj)
-    {
-        Debug.Log("E has been pressed");
-        Destroy(gameObject);
-        Debug.Log("Works");
-        Inventory.instance.AddTrash(1);
+        if (interAction.action.IsPressed() && interactUi == true)
+        {
+            Debug.Log("Button has beeeeeeeen pressed");
+            Destroy(gameObject);
+            Inventory.instance.AddTrash(1);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.CompareTag("Player"))
         {
             Debug.Log("Enter");
-            textInteraction.enabled = true; // affiche le texte PickUp
 
             interactUi = true;
+            // affiche le texte PickUp
 
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
-    {    
+    {
         if (collision.CompareTag("Player"))
         {
             Debug.Log("Exit");
-            textInteraction.enabled = false;
-
             interactUi = false;
             //Unshow text 
         }
--- Game_S34/Assets/Scripts/PlayerUi.cs
+++ Game_S34/Assets/Scripts/PlayerUi.cs
@@ -5,10 +5,12 @@ using TMPro;
 public class CameraControl : MonoBehaviour
 {
     public TextMeshProUGUI textInteraction;
+    public bool interactUi = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Trash") || collision.CompareTag("InteractUI"))
         {
             Debug.Log("Enter");
             textInteraction.enabled = true; // affiche le texte PickUp
@@ -18,7 +20,7 @@ public class CameraControl : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Trash") || collision.CompareTag("InteractUI"))
         {
             Debug.Log("Exit");
             textInteraction.enabled = false;
