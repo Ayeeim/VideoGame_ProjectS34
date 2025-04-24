d633b05a061a061ab92ef9b14affe08dff58d08d
Paillaugue bruno <paillaugue.bruno@gmail.com>
2025-04-24 22:55:11 +0200

    Add Main Menu

---
+++ Game_S34/Assets/MainMenuInteraction.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuInteraction : MonoBehaviour
+{
+    public GameObject panel;
+    public SaveSystem saveSystem;
+
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitter le jeu...");
+
+#if UNITY_EDITOR
+        // Si on est dans l'éditeur Unity
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // Si on est dans un build (PC, Android, etc.)
+        Application.Quit();
+#endif
+    }
+
+    public void newSave()
+    {
+        saveSystem.ResetSave();
+        SceneManager.LoadScene("map");//Changement de scene
+        
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("map");//Changement de scene
+    }
+}
